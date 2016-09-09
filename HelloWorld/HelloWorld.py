c5c66c4ae3a664692cba17dc8ecec8d8294c5432
Piush Vaish <vaishpiush@gmail.com>
2016-09-09 07:12:46 +0100

    first commit

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,20 @@
+# greeting = "hello"
+# name = input("Please enter your name ")
+# print(greeting + ' ' + name)
+#control characters
+# splitString ='This string has been \nsplit over\nseveral\nlines'
+# print(splitString)
+#
+# tabbedString ="1\t2\t3\t"
+# print(tabbedString)
+print('the pet owner said "No, no, \'e \'s uh..he\'s resting" ')
+print("The pet shop owner said \"No, no 'e,'s he's resting\"")
+anotherSplitString = """This string has been
+split over
+several lines"""
+print(anotherSplitString)
+print('''The pet owner said "No,no,'e,'s.uh,...he's resting''')
+
+
+
+
